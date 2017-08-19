ab79bf6db1a91de36c228e402869affd535334b9
Agadar <martin.donkersloot@outlook.com>
2017-08-20 01:42:01 +0200

    Added all validators and templates.

--- src/chat-setting/chat-setting-templates.ts
+++ src/chat-setting/chat-setting-templates.ts
@@ -5,6 +5,32 @@ import * as validators from "./validators";
 // This file contains all ChatSettingTemplates used by ChatSettings to create settings from.
 
 export const ChatSettingTemplates: Array<ChatSettingTemplate<any>> = [
-  new ChatSettingTemplate("running", "Whether the bot is running", false, validators.running, coercers.toBoolean),
-  // new ChatSettingTemplate("firstnotifications", "")
+
+  new ChatSettingTemplate("autoleaderboards",
+    "Whether a leaderboard is posted 1 minute after every dank time",
+    true, validators.autoLeaderboards, coercers.toBoolean),
+
+  new ChatSettingTemplate("firstnotifications", "Whether the first user to score is announced",
+    true, validators.firstNotifications, coercers.toBoolean),
+
+  new ChatSettingTemplate("hardcoreMode", "Whether users who haven't scored in the last 24 hours are punished",
+    false, validators.hardcoreMode, coercers.toBoolean),
+
+  new ChatSettingTemplate("modifier", "Multiplier applied to the score of the first user to score",
+    2, validators.modifier, coercers.toNumber),
+
+  new ChatSettingTemplate("notifications", "Whether notifications are sent for dank times",
+    true, validators.notifications, coercers.toBoolean),
+
+  new ChatSettingTemplate("randomtimepoints", "Points random dank times are worth",
+    10, validators.pointsPerRandomTime, coercers.toWholeNumber),
+
+  new ChatSettingTemplate("randomtimefrequency", "Number of random dank times to generate each day",
+    1, validators.dailyRandomFrequency, coercers.toWholeNumber),
+
+  new ChatSettingTemplate("running", "Whether the bot is running",
+    false, validators.running, coercers.toBoolean),
+
+  new ChatSettingTemplate("timezone", "Timezone users are in", "Europe/Amsterdam",
+    validators.timezone, coercers.toTimezone),
 ];
--- src/chat-setting/validators.ts
+++ src/chat-setting/validators.ts
@@ -2,37 +2,66 @@ import { Validation } from "./validation";
 
 // This file contains all the validators used by ChatSettingTemplates.
 
+export function autoLeaderboards(newValue: boolean, currentValue: boolean): Validation {
+  return {
+    message: newValue ? "Enabled automatic leaderboard posting!"
+      : "Disabled automatic leaderboard posting!", success: true,
+  };
+}
+
 export function dailyRandomFrequency(newValue: number, currentValue: number): Validation {
   if (newValue < 0 || newValue > 24) {
-    return { success: false, message: "The frequency must be a whole number between 0 and 24!" };
+    return { success: false, message: "Frequency must be a whole number between 0 and 24!" };
   }
-  return { success: true, message: "Updated the daily frequency of random dank times!" };
+  return { success: true, message: "Updated daily frequency of random dank times!" };
+}
+
+export function firstNotifications(newValue: boolean, currentValue: boolean): Validation {
+  return {
+    message: newValue ? "Enabled announcements for first users to score!"
+      : "Disabled announcements for first users to score!", success: true,
+  };
+}
+
+export function hardcoreMode(newValue: boolean, currentValue: boolean): Validation {
+  return {
+    message: newValue ? "Enabled hardcore mode! Every day, those who did not score the previous day are punished!"
+      : "Disabled hardcore mode!", success: true,
+  };
 }
 
 export function modifier(newValue: number, currentValue: number): Validation {
   if (newValue < 1 || newValue > 10) {
-    return { success: false, message: "The multiplier must be a number between 1 and 10!" };
+    return { success: false, message: "Multiplier must be a number between 1 and 10!" };
   }
-  return { success: true, message: "Updated the multiplier!" };
+  return { success: true, message: "Updated multiplier!" };
+}
+
+export function notifications(newValue: boolean, currentValue: boolean): Validation {
+  return {
+    message: newValue ? "Enabled normal dank time notifications!"
+      : "Disabled normal dank time notifications! (Random dank time notifications remain enabled.)",
+    success: true,
+  };
 }
 
 export function pointsPerRandomTime(newValue: number, currentValue: number): Validation {
   if (newValue < 1 || newValue > 100) {
-    return { success: false, message: "The points must be a whole number between 1 and 100!" };
+    return { success: false, message: "Points must be a whole number between 1 and 100!" };
   }
-  return { success: true, message: "Updated the points for random dank times!" };
+  return { success: true, message: "Updated points for random dank times!" };
 }
 
 export function running(newValue: boolean, currentValue: boolean): Validation {
   if (newValue === currentValue) {
-    const errorMsg = newValue ? "The bot is already running!" : "The bot is already stopped!";
+    const errorMsg = newValue ? "Bot is already running!" : "Bot is already stopped!";
     return { success: false, message: errorMsg };
   }
-  const successMsg = newValue ? "The bot is now running! Hit '/help' for available commands."
-    : "The bot is now stopped! Hit '/set running true' to restart.";
+  const successMsg = newValue ? "Bot is now running! Hit '/help' for available commands."
+    : "Bot is now stopped! Hit '/set running true' to restart.";
   return { success: true, message: successMsg };
 }
 
 export function timezone(newValue: string, currentValue: string): Validation {
-  return { success: true, message: "Updated the time zone!" };
+  return { success: true, message: "Updated timezone!" };
 }
